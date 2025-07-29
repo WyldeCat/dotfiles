72bcbece3db37ce4e1ce3711dde53fbb5b40d3f6
WyldeCat <skan1543@gmail.com>
2025-07-29 01:41:05 +0000

    feat: add uv

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,25 @@
-# Install nvim
-./install_nvim.sh
 
-# Install nodejs
-./install_nodejs.sh
+if command -v nvim > /dev/null 2>&1; then
+    echo "✅ nvim is already installed: $(command -v nvim)"
+else
+    # Install nvim
+    ./install_nvim.sh
+fi
 
 # Install nvim plugins
 mkdir -p ~/.config
 cp -r ./nvim ~/.config/nvim
+
+if command -v node > /dev/null 2>&1; then
+    echo "✅ node is already installed: $(command -v node)"
+else
+    # Install nodejs
+    ./install_nodejs.sh
+fi
+
+
+if command -v uv > /dev/null 2>&1; then
+    echo "✅ uv is already installed: $(command -v uv)"
+else
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+fi
