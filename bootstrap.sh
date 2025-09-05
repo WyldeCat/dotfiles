e2ed250bf2e15cc38bcacac8dd1b4b5f80c75105
WyldeCat <skan1543@gmail.com>
2025-09-05 04:17:34 +0000

    add lazygit, gh

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,9 +17,20 @@ else
     ./install_nodejs.sh
 fi
 
-
 if command -v uv > /dev/null 2>&1; then
     echo "✅ uv is already installed: $(command -v uv)"
 else
     curl -LsSf https://astral.sh/uv/install.sh | sh
+fi
+
+if command -v lazygit > /dev/null 2>&1; then
+    echo "✅ lazygit is already installed: $(command -v lazygit)"
+else
+    ./install_lazygit.sh
+fi
+
+if command -v gh > /dev/null 2>&1; then
+    echo "✅ gh is already installed: $(command -v gh)"
+else
+    ./install_gh.sh
 fi
